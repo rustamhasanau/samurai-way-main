15311a1451d97319e4484e3e9e068b32a51c2f4b
Rustam Hasanau <rustamhasanau3214@gmail.com>
2022-05-19 23:14:16 +0300

    Change App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,45 @@
 import React from 'react';
 import './App.css';
+/*import Header from "./Header";
+import Technologies from "./Technologies";
+import Footer from "./Footer";*/
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="app-wrapper">
+            <header className='header'>
+                <img
+                    src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/business-logo-design-template-78655edda18bc1196ab28760f1535baa_screen.jpg?ts=1617645324"
+                    alt=""/>
+            </header>
+            <nav className='nav'>
+                <div><a href="">Profile</a></div>
+                <div><a href="">Message</a></div>
+                <div><a href="">News</a></div>
+                <div><a href="">Music</a></div>
+                <div><a href="">Setting</a></div>
+
+            </nav>
+            <div className='content'>
+                <div>
+                    <img src="https://az617363.vo.msecnd.net/imgmodels/albums/academic_centerc38ff2917d951ff9d2733e472d3e80a9_thumb.jpg" alt=""/>
+                </div>
+                <div>ava+discription</div>
+                <div>
+                    <div>
+                        My post
+                        <div>New post</div>
+                        <div>New post</div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Footer.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div className="App">
+            <div>one div</div>
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Footer;
